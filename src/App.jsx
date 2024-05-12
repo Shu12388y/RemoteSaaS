5ace26f29a0420ad3c1d801266f4ab8db8e5331b
karanrana <karanrana3095@gmail.com>
2024-05-12 22:02:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./component/Footer/Footer"
+import Navbar from "./component/Navbar/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
