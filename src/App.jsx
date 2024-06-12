2bf66a0ea1cafa1f42b11f91ac6200cc173b9b3b
karantec <karanrana3095@gmail.com>
2024-06-12 10:46:05 +0530

    darkmodeandlogoupdated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./component/Navbar/Navbar";
 import AboutContent from "./about/AboutContent";
@@ -8,19 +8,38 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import ServiceContent from "./component/Service/ServiceContent";
 import Resources from "./component/Resources/Resource";
 import SignUp from "./signup/Signup";
-
 import Login from "./login/Login";
 import JobDetails from "./component/JobDetail/JobDetails";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import "./App.css"; // Make sure to create this CSS file
 
 const HeroSection = lazy(() => import("./component/HeroSection/HeroSection"));
 import Jobs from "./jobs/Jobs";
 
 const images = ["img/carousel-1.jpg", "img/carousel-2.jpg"];
+
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    setTheme(savedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
   return (
     <ParallaxProvider>
-      <>
-        <Navbar />
+      <div className={`app ${theme}`}>
+        <Navbar className="navbar" />
+        <button className="theme-toggle-button" onClick={toggleTheme}>
+          <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
+        </button>
         <Routes>
           <Route path="/" element={<HeroSection images={images} />} />
           <Route path="/about" element={<AboutContent />} />
@@ -30,10 +49,10 @@ const App = () => {
           <Route path="/resource" element={<Resources />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/jobs/:id" element={<Jobs/>}/>
+          <Route path="/jobs/:id" element={<Jobs />} />
         </Routes>
-        <Footer />
-      </>
+        <Footer className="footer" />
+      </div>
     </ParallaxProvider>
   );
 };
